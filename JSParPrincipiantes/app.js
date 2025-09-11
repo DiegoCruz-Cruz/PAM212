c9f74b31d5540bf4bad873801528ea37a4ffbc1f
DiegoCruz-Cruz <124048797@upq.edu.mx>
2025-09-11 11:02:20 -0600

    Terminamos con el ejemplo de como usar los tipos de datos en la consola del navegador

--- JSParPrincipiantes/app.js
+++ JSParPrincipiantes/app.js
@@ -1,9 +1,19 @@
+// estoy creando variables
+
 var nameuser = "john";
+
 let lastname = "carter";
+
+// reasignsndo el valor de la variable nameuser
 
 nameuser = 'pepe';
 
+// cualquiercosas
+
 const PI = 3.2426;
-PI = 100;
 
 console.log(nameuser)
+
+// camelcase
+
+let userName = 'gordon'

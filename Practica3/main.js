6fce19d51fe9bc4959befae0fd3825df36c9bd60
DiegoCruz-Cruz <124048797@upq.edu.mx>
2025-09-25 20:18:42 -0600

    En este archivo de main, importamos el archivo anterior, mandamos a llamar esa función e imprimimos varios ejemplos para usar la función

---
+++ Practica3/main.js
@@ -0,0 +1,8 @@
+import { restar } from './utils.js';
+
+console.log("Pruebas de la función restar:");
+console.log(restar(10, 5));
+console.log(restar(20, 10));
+console.log(restar(5, 10));
+console.log(restar(0, 0));
+console.log(restar(-5, 5));

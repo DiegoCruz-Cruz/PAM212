25d4e30852c9a8ebe3bdab6d08d4308e3fad003a
DiegoCruz-Cruz <124048797@upq.edu.mx>
2025-10-10 18:52:33 -0600

    En esta práctica aplicamos diferentes estilos a los diferentes componentes del proyecto que ya estábamos realizando, se le aplicaron estilos tanto a textos, variables y botones, y todo esta parte de estilos y de asignación a cada uno de los elementos se tuvo que crear una carpeta nueva y un archivo nuevo de JS para pegar todo el código y en el código base de App.js solo quedara la importación del código y tenerlo más organizado.

--- intro/App.js
+++ intro/App.js
@@ -1,32 +1,9 @@
 //1. Import: Zona de declaraciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button} from 'react-native';
-import React,{useState} from 'react';
+import ContadorScreen from './screens/ContadorScreen'
 
 //2. Main: Zona de componentes
 export default function App() {
-  const[contador,setContador]=useState(0);
-
   return (
-    <View style={styles.container}>
-
-      <Text> Contador: {contador} </Text>
-      <Button title="Incrementar"onPress={()=>setContador(contador+1)}/>
-      <Button title="Quitar"onPress={()=>setContador(contador-1)}/>
-      <Button title="Reiniciar"onPress={()=>setContador(0)}/>
-      <StatusBar style="auto" />
-
-    </View>
-
+    <ContadorScreen></ContadorScreen>
   );
 }
-
-//2. Estilos: Zona de estética y posicionamiento 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

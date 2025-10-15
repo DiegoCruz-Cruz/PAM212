fe18178d14281be7693ac277e2a4bd76a53be297
DiegoCruz-Cruz <124048797@upq.edu.mx>
2025-10-14 19:00:13 -0600

    Se importa el menú de los botones con los que vamos a navegar

--- intro/App.js
+++ intro/App.js
@@ -1,9 +1,9 @@
 //1. Import: Zona de declaraciones
-import ContadorScreen from './screens/ContadorScreen'
+import MenuScreen from './screens/MenuScreen';
 
 //2. Main: Zona de componentes
 export default function App() {
   return (
-    <ContadorScreen></ContadorScreen>
+    <MenuScreen/>
   );
 }
